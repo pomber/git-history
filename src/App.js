eebc1ae25beff6f465b0e42704cbee59b4de2300
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-02-04 18:26:33 -0300

    Fetch content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const repo = "pomber/didact";
+const sha = "master";
+const path = "/src/element.js";
+
+async function getContent(repo, sha, path) {
+  const contentResponse = await fetch(
+    `https://api.github.com/repos/${repo}/contents${path}?ref=${sha}`
+  );
+  const contentJson = await contentResponse.json();
+  const content = window.atob(contentJson.content);
+  // console.log(content);
+  return content;
+}
+
+async function getHistory(repo, sha, path, top = 10) {
+  const commitsResponse = await fetch(
+    `https://api.github.com/repos/${repo}/commits?sha=${sha}&path=${path}`
+  );
+  const commitsJson = await commitsResponse.json();
+  const commits = commitsJson.map(commit => ({
+    sha: commit.sha,
+    author: {
+      // or commiter?
+      login: commit.author.login,
+      avatar: commit.author.avatar_url
+    },
+    url: commit.html_url,
+    message: commit.commit.message
+  }));
+
+  await Promise.all(
+    commits.slice(0, top).map(async commit => {
+      commit.content = await getContent(repo, commit.sha, path);
+    })
+  );
+
+  return commits;
+}
+
+getHistory(repo, sha, path).then(console.log);
 
 class App extends Component {
   render() {
