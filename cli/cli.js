456dc408e6674419ced126eb6b516019c1d1a2de
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-02-10 17:19:11 -0300

    Add validations

--- cli/cli.js
+++ cli/cli.js
@@ -2,8 +2,22 @@
 
 const runServer = require("./server");
 const getCommits = require("./git");
+const fs = require("fs");
 
-const path = process.argv.slice(2);
+const path = process.argv[2];
+
+if (!path || path === "--help") {
+  console.log(`Usage:
+
+  githistory some/file.ext
+  `);
+  process.exit();
+}
+
+if (!fs.existsSync(path)) {
+  console.log(`File not found: ${path}`);
+  process.exit();
+}
 
 const commitsPromise = getCommits(path);
 
