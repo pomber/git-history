c7e7b8546b1b8efc39d2cab861e8e16cf6080617
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-03-14 17:59:58 -0300

    Add double scroller story

--- src/scroller.story.js
+++ src/scroller.story.js
@@ -2,19 +2,32 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Scroller from "./scroller";
 
-const snapAreas = [
+const snapAreas1 = [
   { start: 1, end: 5 },
   { start: 15, end: 26 },
   { start: 50, end: 100 },
   { start: 300, end: 302 }
 ];
 
-const items = Array(500)
+const snapAreas2 = [
+  { start: 8, end: 12 },
+  { start: 30, end: 32 },
+  { start: 550, end: 552 }
+];
+
+const items = Array(600)
   .fill(0)
   .map((_, i) => {
-    const a = snapAreas.find(a => a.start <= i && i <= a.end);
+    const a1 = snapAreas1.find(a => a.start <= i && i <= a.end);
+    const a2 = snapAreas2.find(a => a.start <= i && i <= a.end);
     return {
-      content: `Row ${i}${a ? ` - Area [${a.start}, ${a.end}]` : ""}`,
+      content: `Row ${i}${
+        a1
+          ? ` - Area1 [${a1.start}, ${a1.end}]`
+          : a2
+          ? ` - Area2 [${a2.start}, ${a2.end}]`
+          : ""
+      }`,
       key: i,
       height: 22
     };
@@ -32,12 +45,12 @@ function getRowHeight(item) {
   return item.height;
 }
 
-function BasicScroller() {
+function BasicScroller({ areas }) {
   const [top, setTop] = React.useState(40);
   return (
     <Scroller
       items={items}
-      snapAreas={snapAreas}
+      snapAreas={areas}
       getRow={getRow}
       getRowHeight={getRowHeight}
       top={top}
@@ -46,7 +59,7 @@ function BasicScroller() {
   );
 }
 
-storiesOf("Scroller", module).add("test", () => (
+storiesOf("Scroller", module).add("single", () => (
   <div
     style={{
       display: "flex",
@@ -56,7 +69,35 @@ storiesOf("Scroller", module).add("test", () => (
     }}
   >
     <div style={{ width: "60%", height: "80vh", border: "1px solid black" }}>
-      <BasicScroller />
+      <BasicScroller areas={snapAreas1} />
     </div>
+  </div>
+));
+
+function DoubleScroller() {
+  const [flag, setFlag] = React.useState(false);
+  return (
+    <div>
+      <div style={{ height: "80vh", border: "1px solid black", width: "60vw" }}>
+        <BasicScroller areas={flag ? snapAreas1 : snapAreas2} />
+      </div>
+      <div>
+        {flag ? "Areas 1" : "Areas 2"}
+        <button onClick={() => setFlag(flag => !flag)}>Toggle</button>
+      </div>
+    </div>
+  );
+}
+
+storiesOf("Scroller", module).add("multiple", () => (
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "90vh"
+    }}
+  >
+    <DoubleScroller />
   </div>
 ));
